6ef0e1a1a0789e058137a781a260b0277edcb11b
Edward <gulyaev.ed.v@gmail.com>
2021-06-29 23:04:16 +0400

    Add min ui
    Add button, icon

--- TaskManager/MainWindow.xaml.cs
+++ TaskManager/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TaskManager
         {
             InitializeComponent();
         }
+
+        private void miExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
