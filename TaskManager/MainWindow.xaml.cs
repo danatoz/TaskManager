d827eccbac61ee0a2ca3bae485a8e5adf21da200
Edward <gulyaev.ed.v@gmail.com>
2021-06-30 23:20:28 +0400

    Modify UI

--- TaskManager/MainWindow.xaml.cs
+++ TaskManager/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TaskManager.Mock;
+using TaskManager.Model;
 
 namespace TaskManager
 {
@@ -20,9 +23,15 @@ namespace TaskManager
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Model.Task> CollectionTasks;
         public MainWindow()
         {
             InitializeComponent();
+            CollectionTasks = new ObservableCollection<Model.Task>();
+            lbAllTask.ItemsSource = CollectionTasks;
+            CollectionTasks.Add(
+                new Model.Task() { HeaderTask = "Hello world!", TaskDone = false, RepeatTask = true}
+            );
         }
 
         private void miExit_Click(object sender, RoutedEventArgs e)
---
+++ TaskManager/Model/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManager.Model
+{
+    public class Task
+    {
+        private string dateTime;
+
+        public bool TaskDone { get; set; }
+        public string HeaderTask { get; set; }
+        public string DateOfCreation
+        {
+            get => dateTime;
+            set { value = dateTime = DateTime.Now.ToString(); }
+        }
+        public string DeadLine { get; set; }
+        public bool RepeatTask { get; set; }
+    }
+}
