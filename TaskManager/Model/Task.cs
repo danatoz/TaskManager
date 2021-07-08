e54ae5fbcd8ca979ece10736d7b892d2eeec81fd
Edward <gulyaev.ed.v@gmail.com>
2021-07-09 00:48:55 +0400

    Add mvvm

--- TaskManager/Model/Task.cs
+++ TaskManager/Model/Task.cs
@@ -1,23 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TaskManager.ViewModel;
 
 namespace TaskManager.Model
 {
-    public class Task
+    public class Task : BaseNotifyProperty
     {
-        private string dateTime;
+
+
+        private string _dateTime;
+        private string _headerTask;
+        private bool _taskDone;
+
         public int Id { get; set; }
-        public bool TaskDone { get; set; }
-        public string HeaderTask { get; set; }
+
+        public bool TaskIsDone
+        {
+            get => _taskDone;
+            set
+            {
+                _taskDone = value;
+                OnPropertyChanged("TaskIsDone");
+            }
+        }
+
+        public string HeaderTask
+        {
+            get => _headerTask;
+            set
+            {
+                _headerTask = value;
+                OnPropertyChanged("HeaderTask");
+            }
+        }
+
         public string DateOfCreation
         {
-            get => dateTime;
-            set { value = dateTime = DateTime.Now.ToString(); }
+            get => _dateTime;
+            set
+            {
+                _dateTime = value;
+                OnPropertyChanged("DateOfCreation");
+            }
         }
         public string DeadLine { get; set; }
         public bool RepeatTask { get; set; }
+        public bool TaskFavorite { get; set; }
 
-        //public List<Task> Completed { get; set; }
     }
 }
