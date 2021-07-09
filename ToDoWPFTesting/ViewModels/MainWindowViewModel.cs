a94f8b43a0c7852bfb4d2206414fd4a006eb0955
Edward <gulyaev.ed.v@gmail.com>
2021-07-09 21:26:48 +0400

    Test

---
+++ ToDoWPFTesting/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ToDoWPFTesting.ViewModels.Base;
+
+namespace ToDoWPFTesting.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _title = "Hello world!";
+
+        public string Title
+        {
+            get => _title;
+            set => Set(ref _title, value);
+        }
+    }
+}
